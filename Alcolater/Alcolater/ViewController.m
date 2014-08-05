138ef18b66fb06a1544aa267a27c74f2ece34da4
Kervins Valcourt <kervins.valcourt@gmail.com>
2014-08-05 15:37:25 -0400

    custom view controller

--- Alcolater/Alcolater/ViewController.m
+++ Alcolater/Alcolater/ViewController.m
@@ -2,7 +2,7 @@
 //  ViewController.m
 //  Alcolater
 //
-//  Created by Kervins Valcourt on 7/31/14.
+//  Created by Kervins Valcourt  on 7/31/14.
 //  Copyright (c) 2014 EastoftheWestEnd. All rights reserved.
 //
 
